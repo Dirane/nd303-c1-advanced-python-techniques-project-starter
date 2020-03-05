a66ed5a667adbc272dcecc0f55a810b977947b8a
dirane <Dirane@macs-MacBook-Pro.local>
2020-03-05 04:10:37 +0100

    Designing objects

--- starter/models.py
+++ starter/models.py
@@ -10,6 +10,13 @@ class NearEarthObject(object):
         :param kwargs:    dict of attributes about a given Near Earth Object, only a subset of attributes used
         """
         # TODO: What instance variables will be useful for storing on the Near Earth Object?
+        self.name = kwargs.get('name', 'No name gotten')
+        self.id = kwargs.get('id', None)
+        self.diameter_min_km = float(kwargs.get(
+            'estimated_diameter_min_kilometers', 0))
+        self.is_potentially_hazardous_asteroid = kwargs.get(
+            'is_potentially_hazardous_asteroid', False)
+        self.orbits = []
 
     def update_orbits(self, orbit):
         """
@@ -20,6 +27,17 @@ class NearEarthObject(object):
         """
 
         # TODO: How do we connect orbits back to the Near Earth Object?
+        self.orbits.append(orbit)
+
+    def __repr__(self):
+        """returns information about the object for debugging purposes"""
+
+        return f'NearEarthObject id:{self.id} name:{self.name} orbit_dates:{[orbit.close_approach_date for orbit in self.orbits]} harzardous_asteriod:{self.is_potentially_hazardous_asteroid}'
+
+    def __str__(self):
+        """Prints information about the object"""
+
+        return f'NearEarthObject id:{self.id} name:{self.name} orbit_dates:{[orbit.close_approach_date for orbit in self.orbits]} harzardous_asteriod:{self.is_potentially_hazardous_asteroid}'
 
 
 class OrbitPath(object):
@@ -34,3 +52,12 @@ class OrbitPath(object):
         :param kwargs:    dict of attributes about a given orbit, only a subset of attributes used
         """
         # TODO: What instance variables will be useful for storing on the Near Earth Object?
+        self.neo_name = kwargs.get('name', 'No name gotten')
+        self.close_approach_date = kwargs.get('close_approach_date', None)
+        self.miss_distance_kilometers = float(
+            kwargs.get('miss_distance_kilometers', 0))
+
+    def __str__(self):
+        """returns information about the object"""
+
+        return f'OrbitPath neo_name:{self.neo_name} close_approach_date:{self.close_approach_date} missed_distance_kilometers:{self.miss_distance_kilometers}'
